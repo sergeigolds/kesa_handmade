4559f7edb6283f2c57ea678d83aec5f7041bb66b
Sergei Golds <sergei.golds@gmail.com>
2020-05-17 13:13:57 +0300

    templates: header, footer, base

--- products/views.py
+++ products/views.py
@@ -15,4 +15,4 @@ class ProductDetailView(View):
     """Products list"""
     def get(self, request, slug):
         product = Product.objects.get(url=slug)
-        return render(request, 'products/single-product-details.html', {'product': product})
+        return render(request, 'products/single_product_details.html', {'product': product})
